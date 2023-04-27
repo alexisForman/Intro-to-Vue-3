d92932e0719d61aa40deec3a9fc1e5654d56a4f4
alexisForman <alexisgforman@gmail.com>
2023-04-27 14:39:38 -0500

    end of lesson 1 databinding understood

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function()    {
+        return  {
+            product: 'Socks',
+            description: 'Warm and fuzzy!'
+        }
+    }
+})
+
+//The shorthand version of this is:
+// const app = Vue.createApp({
+//     data()    {
+//         return  {
+//             product: 'Socks'
+//         }
+//     }
+// })
+
